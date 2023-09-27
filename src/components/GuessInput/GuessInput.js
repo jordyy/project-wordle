98e68d4712f35cc93a0466bb244a78d8318c5668
Jordy <jordynavalee@gmail.com>
2023-09-27 13:33:36 -0500

    adds min-max logic

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -4,12 +4,22 @@ function GuessInput() {
   const [guess, setGuess] = React.useState("");
 
   function handleChange(event) {
-    setGuess(event.target.value);
+    const capGuess = event.target.value.toUpperCase();
+    if (capGuess.length > 5) {
+      window.alert("guess cannot exceed 5 characters");
+      return;
+    }
+    setGuess(capGuess);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
+    if (guess.length < 5) {
+      window.alert("guess must be 5 characters");
+      return;
+    }
     console.log(guess);
+    setGuess("");
   }
 
   return (
