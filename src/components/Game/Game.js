ec0c611fd4bb44c1391c612213a0efb07b417eb3
Jordy <jordynavalee@gmail.com>
2023-09-29 15:04:43 -0500

    adds win/lose logic and indicator for user

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,7 +4,7 @@ import { sample } from "../../utils";
 import { WORDS } from "../../data";
 import GuessInput from "../GuessInput/GuessInput";
 import GuessResults from "../GuessResults/GuessResults";
-import Guess from "../Guess/Guess";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -13,12 +13,36 @@ console.info({ answer });
 
 function Game() {
   const [guesses, setGuesses] = React.useState([]);
+  const [gameStatus, setGameStatus] = React.useState("running");
 
-  function handleSubmitGuess(tentativeGuess) {
+  function handleSubmitGuess(tentativeGuess, num) {
     setGuesses([...guesses, tentativeGuess]);
+
+    if (tentativeGuess === answer) {
+      setGameStatus("won");
+    } else if (guesses[num] >= NUM_OF_GUESSES_ALLOWED) {
+      setGameStatus("lost");
+    }
   }
+
   return (
     <>
+      {gameStatus === "won" && (
+        <div class="happy banner">
+          <p>
+            <strong>Congratulations!</strong> Got it in
+            <strong> {guesses.length} guesses</strong>.
+          </p>
+        </div>
+      )}
+      {gameStatus === "lost" && (
+        <div class="sad banner">
+          <p>
+            Sorry, the correct answer is <strong>{answer}</strong>.
+          </p>
+        </div>
+      )}
+
       <GuessResults guesses={guesses} answer={answer} />
       <GuessInput handleSubmitGuess={handleSubmitGuess} />
     </>
